02d6284a7abc7f171a8867f0f9f9c56177e97f35
freehost <67769695+facebiik@users.noreply.github.com>
2022-11-04 10:29:24 -0600

    Create sample_installer.php

---
+++ install/cli/sample_installer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Sample cli installer script
+ */
+
+// change to true to run this script. Change back to false when done.
+$enabled = false;
+
+// none of the following may be empty
+$params = [
+	// database parameters
+	'dbuser' => '',
+	'dbpassword' => '',
+	'dbname' => '',
